a1b6d715e870a25a666f17365c82a040e75cd0dd
ALittlePatate <maxime@patate.dev>
2024-01-20 18:10:00 +0100

    add: documented pasm_run_script function

--- src/pasm.c
+++ src/pasm.c
@@ -66,6 +66,11 @@ int check_errors(char *line) {
     return 0;
 }
 
+/* filename: path of the pasm script, will use file argument if NULL                    */
+/* file: char** containing the lines of a pasm script, will be used if filename is NULL */
+/* lines: number of lines in the file argument, will be used if filename is NULL        */
+/* _fstream: file descriptor for the output (can be a socket), default: stderr          */
+/* return value: nonzero if an error occurred                                           */
 int pasm_run_script(const char *filename, char **file, size_t lines, int _fstream) {
     fstream = _fstream;
 
